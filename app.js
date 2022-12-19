a078b6e5041e46706390b72670f1c615dda0ab11
ATISH YADAV <atishcomp@gmail.com>
2022-12-19 15:38:15 +0530

    database connected

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 var express =require("express");
+require('dotenv').config();
 var app= express();
+const mongoose = require("mongoose");
+var cors = require("cors");
+var bodyParser= require("body-parser");
+
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+
+//Using environmnet variable to secure the connection password of db
+//we are using mongodb cloud atlas database
+
+mongoose.connect(process.env.DATABASE_CONNECTION,{
+    
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    }).then(()=>{
+        console.log("DB CONNECTED") 
+    }).catch((error)=>console.log(error.message))
 
 app.get('/',(req,res)=>{
     res.send("WELCOME TO HACKSCRIPT BACKEND SERVICE")
